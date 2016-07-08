0cf395d6cc8568f2f6d844eca187bdaebd87d0e7
Marin Atanasov Nikolov <dnaeon@gmail.com>
2016-07-08 10:55:05 +0300

    tests

---
+++ file.go
@@ -0,0 +1,18 @@
+package main
+
+// File type.
+// Another line.
+// And one more.
+//
+// Example:
+//  f := &File{
+//    A: "a-field",
+//    B: "b-field",
+//  }
+type File struct {
+	// A field doc
+	A string
+
+	// B field doc
+	B string
+}
